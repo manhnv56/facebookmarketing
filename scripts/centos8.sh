a7cb6a456f037af745fe44a095845c040ecbe5b2
manhnv56 <manhnv_56@vnu.edu.vn>
2021-11-12 19:17:51 +0700

    Update centos8.sh

--- scripts/centos8.sh
+++ scripts/centos8.sh
@@ -46,7 +46,7 @@ daemon
 maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
-nserver 2001:4860:4860::8888
+nserver 2606:4700:4700::1111
 nserver 2001:4860:4860::8844
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
